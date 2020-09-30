7cc35a0408f05102c5960f4935ccdc2e973bac26
Carrington <55025157+mcarringtone@users.noreply.github.com>
2020-09-30 17:39:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let button
+
+button = document.querySelector("button");
+
+button.addEventListener("click", changeColor)
+let hexColors=["#fcba03","#fc0b03","#e6605c","#b19eff","#ff9edd"];
+document.body.style.backgroundColor = "#ed7d6d"
+
+
+function changeColor (){
+  let hexColors=["#fcba03","#fc0b03","#e6605c","#b19eff","#ff9edd"];
+  let hexColor=hexColors[Math.floor(Math.random()*hexColors.length )];
+
+  document.body.style.background = hexColor;
+  document.getElementById("text-uppercase").innerHTML = "Hex Color " + hexColor;
+
+  
+};
